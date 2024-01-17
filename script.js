204f3102409c35ac7a19454f78232fdbaecfbb17
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-01-17 22:49:10 +0530

    Add winner function

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ const winPatterns = [
     [3,4,5],
     [6,7,8]
 ]
+
+boxes.forEach((box)=>{
+    box.addEventListener('click', ()=>{
+        if(turnO){
+            box.innerText = 'O';
+            turnO = false;
+        }
+        else{
+            box.innerText = 'X';
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const checkWinner = ()=>{
+    for(let patterns of winPatterns){
+        let pos1Val = boxes[patterns[0]].innerText;
+        let pos2Val = boxes[patterns[1]].innerText;
+        let pos3Val = boxes[patterns[2]].innerText;
+        
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != " "){
+           if(pos1Val === pos2Val && pos2Val === pos3Val){
+              console.log('winner');
+           }
+        }
+
+        
+    }
+}
